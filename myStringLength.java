43af123be7d4fff5d3ab317bde367aa897d92ba4
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-01-04 09:56:31 -0800

    Change string to 'My name is Alina Stewart.'

--- myStringLength.java
+++ myStringLength.java
@@ -2,7 +2,7 @@
 
 public class myStringLength {
   public static void main(String[] args) {
-    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String txt = "My name is Alina Stewart.";
     System.out.println("The length of the txt string is: " + txt.length());
   }
 }
